d4dc985f50316efb99760c628190f7a6cfb86af6
Beloborod <const.bpa@gmail.com>
2020-06-16 16:24:47 +0700

    добавлен готовый проект автоюрист

--- lawyer/autoyurist/assets/js/script.js
+++ lawyer/autoyurist/assets/js/script.js
@@ -1,5 +1,5 @@
 /*navbar*/
-jQuery('.b-navbar-hamburger').click(function() {
+jQuery('.b-navbar-hamburger').click(function () {
     jQuery(this).toggleClass('b-navbar-hamburger_active');
 });
 
@@ -10,33 +10,33 @@ jQuery('.b-reports-slick').slick({
     mobileFirst: true,
 
     responsive: [{
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                arrows: true,
-                prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/img/site/slider-left.png'>",
-                nextArrow: "<img class='a-right control-c next slick-next' src='./assets/img/site/slider-right.png'>"
-            }
-        },
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            arrows: true,
+            prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/img/site/slider-left.png'>",
+            nextArrow: "<img class='a-right control-c next slick-next' src='./assets/img/site/slider-right.png'>"
+        }
+    },
 
 
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                arrows: true,
-                prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/img/site/slider-left.png'>",
-                nextArrow: "<img class='a-right control-c next slick-next' src='./assets/img/site/slider-right.png'>"
-            }
-        },
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: true,
+            prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/img/site/slider-left.png'>",
+            nextArrow: "<img class='a-right control-c next slick-next' src='./assets/img/site/slider-right.png'>"
+        }
+    },
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
         // instead of a settings object
@@ -55,134 +55,134 @@ function initMap() {
         mapTypeControl: false,
         streetViewControl: false,
         styles: [{
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#E6E6E6"
-                }]
-            },
-            {
-                "elementType": "labels.icon",
-                "stylers": [{
-                    "color": "#656565"
-                }]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#616161"
-                }]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "color": "#E6E6E6"
-                }]
-            },
-            {
-                "featureType": "administrative.land_parcel",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#bdbdbd"
-                }]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#eeeeee"
-                }]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#757575"
-                }]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#e5e5e5"
-                }]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#9e9e9e"
-                }]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }]
-            },
-            {
-                "featureType": "road",
-                "elementType": "labels",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            },
-            {
-                "featureType": "road.arterial",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#757575"
-                }]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#dadada"
-                }]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#616161"
-                }]
-            },
-            {
-                "featureType": "road.local",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#9e9e9e"
-                }]
-            },
-            {
-                "featureType": "transit.line",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#e5e5e5"
-                }]
-            },
-            {
-                "featureType": "transit.station",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#eeeeee"
-                }]
-            },
-            {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#C3C3C3"
-                }]
-            },
-            {
-                "featureType": "water",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#9e9e9e"
-                }]
-            }
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#E6E6E6"
+            }]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [{
+                "color": "#656565"
+            }]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#616161"
+            }]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "color": "#E6E6E6"
+            }]
+        },
+        {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#bdbdbd"
+            }]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#eeeeee"
+            }]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#757575"
+            }]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#e5e5e5"
+            }]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#9e9e9e"
+            }]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#ffffff"
+            }]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#757575"
+            }]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#dadada"
+            }]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#616161"
+            }]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#9e9e9e"
+            }]
+        },
+        {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#e5e5e5"
+            }]
+        },
+        {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#eeeeee"
+            }]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#C3C3C3"
+            }]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#9e9e9e"
+            }]
+        }
         ]
     });
 
@@ -198,3 +198,19 @@ function initMap() {
     });
 
 }
+
+let bAmenitiesCard = document.querySelectorAll('.b-amenities__card');
+
+bAmenitiesCard.forEach(element => {
+    element.addEventListener("click", (event) => {
+        let target = event.target;
+        if (target.classList.contains("b-amenities__card-title")) {
+            element.querySelector(".b-amenities__card-content").classList.toggle('b-amenities__card-conten-active');
+        }
+        if (target.classList.contains("b-amenities__card-btn")){
+            document.querySelector('#call').scrollIntoView({block: "center", behavior: "smooth"})
+            console.log(document.querySelector('#call'));
+        }
+    })
+});
+
