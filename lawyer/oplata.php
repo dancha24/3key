81160963057e236bbea8c64865ab837b842b9f8b
daniil <dancha241@gmail.com>
2020-09-02 20:17:19 +0300

    тестирование енота frff

--- lawyer/oplata.php
+++ lawyer/oplata.php
@@ -113,6 +113,16 @@
         </div>
     </div>
 </nav>-->
+
+<?php
+         $MERCHANT_ID   = 15;                 // ID магазина
+         $SECRET_WORD   = 'Секретный ключ';   // Секретный ключ
+         $ORDER_AMOUNT  = 10;                 // Сумма заказа
+         $PAYMENT_ID    = time();             // ID заказа (мы используем time(), чтобы был всегда уникальный ID)
+
+         $sign = md5($MERCHANT_ID.':'.$ORDER_AMOUNT.':'.$SECRET_WORD.':'.$PAYMENT_ID);  //Генерация ключа
+         ?>
+
 <form method='get' action='https://enot.io/pay'>
     <input type='hidden' name='m' value='<?=$MERCHANT_ID?>'>
     <input type='hidden' name='oa' value='<?=$ORDER_AMOUNT?>'>
