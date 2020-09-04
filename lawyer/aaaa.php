22b69006160b45d7a344f7c3707f98cc48878a8a
daniil <dancha241@gmail.com>
2020-09-04 14:39:23 +0300

    Редактирование калькулятора для сайта банкротства

--- lawyer/aaaa.php
+++ lawyer/aaaa.php
@@ -12,6 +12,38 @@ $options = array(
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 if ($result === FALSE) { /* Handle error */ }
-
-var_dump($result);
 ?>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="description"
+          content="3Кей, юридические услуги">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Юридические услуги Кемерово">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>3Кей</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/f3bca98bf0.js" crossorigin="anonymous"></script>
+
+
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="css/agency.min.css" rel="stylesheet">
+</head>
+<body>
+<div align="center" class="row justify-content-center">
+    <div class="col-12 align-items-center">
+        <h1>Заявка отправленна</h1>
+        <p>Наш юрист свяжется с вами в ближайшее время</p>
+        <a href="https://xn--e1afec2al.xn--p1ai/bankrotam" ><button class="btn btn-xl btn-success">Вернуться на главную</button></a>
+    </div>
+</div>
+</body>
